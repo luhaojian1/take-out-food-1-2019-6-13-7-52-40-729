d58b3b6e65121ad0566f3ec44a8c5883a23d97ff
luhaojian11 <962671114@qq.com>
2019-07-11 01:09:05 +0800

    finish isPromotion method

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,26 +1,26 @@
 const takeOutFood = require("../src/best-charge.js");
 describe('Take out food', function () {
 
-  it('should return an arrangeItem when arrangeItems given an array', function() {
+  it('should return an arrangeItem when arrangeItems given an array', function () {
     //given
     let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
     //when
     let items = takeOutFood.countItems(inputs);
     //then
-    let expected = [ { itemId: 'ITEM0001', count: 1 },
-      { itemId: 'ITEM0013', count: 2 },
-      { itemId: 'ITEM0022', count: 1 } ];
+    let expected = [{itemId: 'ITEM0001', count: 1},
+      {itemId: 'ITEM0013', count: 2},
+      {itemId: 'ITEM0022', count: 1}];
     expect(items).toEqual(expected);
   });
 
   // getItem test
-  it('should return an item Object when getItem given itemId = \'ITEM0001\'', function() {
+  it('should return an item Object when getItem given itemId = \'ITEM0001\'', function () {
     //given
     let inputs = 'ITEM0001';
     //when
     let items = takeOutFood.getItem(inputs);
     //then
-    let expected ={
+    let expected = {
       id: 'ITEM0001',
       name: '黄焖鸡',
       price: 18.00
@@ -28,7 +28,7 @@ describe('Take out food', function () {
     expect(items).toEqual(expected);
   });
 
-  it('should return null when getItem given itemId = \'ITEM0000\'', function() {
+  it('should return null when getItem given itemId = \'ITEM0000\'', function () {
     //given
     let inputs = 'ITEM0000';
     //when
@@ -36,4 +36,23 @@ describe('Take out food', function () {
     //then
     expect(item).toBe(null);
   });
+
+  // isPromotion test
+  it('should return false when isPromotion given itemId = \'ITEM0000\'', function () {
+    //given
+    let inputs = 'ITEM0000';
+    //when
+    let item = takeOutFood.isPromotion(inputs);
+    //then
+    expect(item).toBe(false);
+  });
+  it('should return true when isPromotion given itemId = \'ITEM0001\'', function () {
+    //given
+    let inputs = 'ITEM0001';
+    //when
+    let item = takeOutFood.isPromotion(inputs);
+    //then
+    expect(item).toBe(true);
+  });
+
 });
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,16 +1,18 @@
 const loadAllItems = require("./items");
 const loadPromotions = require("./promotions");
+
 function bestCharge(selectedItems) {
   return /*TODO*/;
 }
+
 //计算商品数目
 function countItems(selectedItems) {
   let items = [];
-  selectedItems.forEach( item =>{
+  selectedItems.forEach(item => {
     let itemInfo = item.split("x");
     items.push({
-      itemId:itemInfo[0].trim(),
-      count:parseInt(itemInfo[1].trim())
+      itemId: itemInfo[0].trim(),
+      count: parseInt(itemInfo[1].trim())
     });
   });
   console.log(items);
@@ -18,16 +20,28 @@ function countItems(selectedItems) {
 }
 
 //获取商品
-function getItem(itemId){
+function getItem(itemId) {
   const items = loadAllItems();
-  for (let i = 0; i < items.length; i++){
-    if (items[i].id === itemId){
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].id === itemId) {
       return items[i];
     }
   }
   return null;
 }
+
+function isPromotion(itemId) {
+  const promotions = loadPromotions();
+  const promotionItems = promotions[1].items;
+  for (let i = 0; i < promotionItems.length; i++)  {
+    if (promotionItems[i] === itemId)
+      return true;
+  }
+  return false;
+}
+
+
 module.exports = {
-  countItems,getItem
+  countItems, getItem,isPromotion,
 
 }
