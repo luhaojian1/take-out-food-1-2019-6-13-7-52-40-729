2e7ddce813a750dab4ea6ccc911c173a4af4ef52
luhaojian11 <962671114@qq.com>
2019-07-10 23:48:17 +0800

    finish getItem method

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -3,7 +3,7 @@ describe('Take out food', function () {
 
   it('should return an arrangeItem when arrangeItems given an array', function() {
     //given
-    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];;
+    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
     //when
     let items = takeOutFood.countItems(inputs);
     //then
@@ -11,5 +11,29 @@ describe('Take out food', function () {
       { itemId: 'ITEM0013', count: 2 },
       { itemId: 'ITEM0022', count: 1 } ];
     expect(items).toEqual(expected);
+  });
+
+  // getItem test
+  it('should return an item Object when getItem given itemId = \'ITEM0001\'', function() {
+    //given
+    let inputs = 'ITEM0001';
+    //when
+    let items = takeOutFood.getItem(inputs);
+    //then
+    let expected ={
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18.00
+    };
+    expect(items).toEqual(expected);
+  });
+
+  it('should return null when getItem given itemId = \'ITEM0000\'', function() {
+    //given
+    let inputs = 'ITEM0000';
+    //when
+    let item = takeOutFood.getItem(inputs);
+    //then
+    expect(item).toBe(null);
   });
 });
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,7 +1,9 @@
+const loadAllItems = require("./items");
+const loadPromotions = require("./promotions");
 function bestCharge(selectedItems) {
   return /*TODO*/;
 }
-
+//计算商品数目
 function countItems(selectedItems) {
   let items = [];
   selectedItems.forEach( item =>{
@@ -14,7 +16,18 @@ function countItems(selectedItems) {
   console.log(items);
   return items;
 }
+
+//获取商品
+function getItem(itemId){
+  const items = loadAllItems();
+  for (let i = 0; i < items.length; i++){
+    if (items[i].id === itemId){
+      return items[i];
+    }
+  }
+  return null;
+}
 module.exports = {
-  countItems,
+  countItems,getItem
 
 }
