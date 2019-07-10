099d480ede1e39e88cc113bb5852181affe5f75a
luhaojian11 <962671114@qq.com>
2019-07-10 23:37:03 +0800

    finish countItems method

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,48 +1,15 @@
+const takeOutFood = require("../src/best-charge.js");
 describe('Take out food', function () {
 
-  it('should generate best charge when best is 指定菜品半价', function() {
-    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
-============= 订餐明细 =============
-黄焖鸡 x 1 = 18元
-肉夹馍 x 2 = 12元
-凉皮 x 1 = 8元
------------------------------------
-使用优惠:
-指定菜品半价(黄焖鸡，凉皮)，省13元
------------------------------------
-总计：25元
-===================================`.trim()
-    expect(summary).toEqual(expected)
+  it('should return an arrangeItem when arrangeItems given an array', function() {
+    //given
+    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];;
+    //when
+    let items = takeOutFood.countItems(inputs);
+    //then
+    let expected = [ { itemId: 'ITEM0001', count: 1 },
+      { itemId: 'ITEM0013', count: 2 },
+      { itemId: 'ITEM0022', count: 1 } ];
+    expect(items).toEqual(expected);
   });
-
-  it('should generate best charge when best is 满30减6元', function() {
-    let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
-============= 订餐明细 =============
-肉夹馍 x 4 = 24元
-凉皮 x 1 = 8元
------------------------------------
-使用优惠:
-满30减6元，省6元
------------------------------------
-总计：26元
-===================================`.trim()
-    expect(summary).toEqual(expected)
-  });
-
-  it('should generate best charge when no promotion can be used', function() {
-    let inputs = ["ITEM0013 x 4"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
-============= 订餐明细 =============
-肉夹馍 x 4 = 24元
------------------------------------
-总计：24元
-===================================`.trim()
-    expect(summary).toEqual(expected)
-  });
-
 });
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,20 @@
 function bestCharge(selectedItems) {
   return /*TODO*/;
 }
+
+function countItems(selectedItems) {
+  let items = [];
+  selectedItems.forEach( item =>{
+    let itemInfo = item.split("x");
+    items.push({
+      itemId:itemInfo[0].trim(),
+      count:parseInt(itemInfo[1].trim())
+    });
+  });
+  console.log(items);
+  return items;
+}
+module.exports = {
+  countItems,
+
+}
